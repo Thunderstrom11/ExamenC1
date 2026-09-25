c902e6fc5821de03442bafc6ba2735bfe5d3a935
Diego Chamendy <dmchamendy@uamv.edu.ni>
2026-09-25 00:16:01 -0600

    programa arreglado y completo con los requisitos del enunciado

--- src/main/java/ni/edu/uam/examenc1/Launcher.java
+++ src/main/java/ni/edu/uam/examenc1/Launcher.java
@@ -5,7 +5,6 @@ import ni.edu.uam.examenc1.application.RegistroApplication;
 
 public class Launcher {
     public static void main(String[] args) {
-        // punto de entrada: evita problemas de modulos al correr desde el IDE
         Application.launch(RegistroApplication.class, args);
     }
 }
